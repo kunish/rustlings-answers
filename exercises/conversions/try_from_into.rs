cac3ce2aa1ebd53c5f61d33e93d4b4c725fbf830
shikun <kunish.butt@gmail.com>
2022-05-07 23:27:47 +0800

    feat: save point

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -21,8 +21,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -32,10 +30,23 @@ enum IntoColorError {
 // but the slice implementation needs to check the slice length!
 // Also note that correct RGB color values must be integers in the 0..=255 range.
 
+fn is_color_value_in_rgb_range(colors: Vec<i16>) -> bool {
+    colors.into_iter().all(|x| x >= 0 && x <= 255)
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if is_color_value_in_rgb_range(vec![tuple.0, tuple.1, tuple.2]) {
+            return Ok(Self {
+                red: tuple.0 as u8,
+                green: tuple.1 as u8,
+                blue: tuple.2 as u8,
+            });
+        }
+
+        Err(Self::Error::IntConversion)
     }
 }
 
@@ -43,6 +54,15 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if is_color_value_in_rgb_range(vec![arr[0], arr[1], arr[2]]) {
+            return Ok(Self {
+                red: arr[0] as u8,
+                green: arr[1] as u8,
+                blue: arr[2] as u8,
+            });
+        }
+
+        Err(Self::Error::IntConversion)
     }
 }
 
@@ -50,6 +70,19 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(Self::Error::BadLen);
+        }
+
+        if is_color_value_in_rgb_range(vec![slice[0], slice[1], slice[2]]) {
+            return Ok(Self {
+                red: slice[0] as u8,
+                green: slice[1] as u8,
+                blue: slice[2] as u8,
+            });
+        } else {
+            return Err(Self::Error::IntConversion);
+        }
     }
 }
 
--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -10,7 +10,7 @@ pub fn generate_nametag_text(name: String) -> Result<String, String> {
         Ok(format!("Hi! My name is {}", name))
     } else {
         // Empty names aren't allowed.
-        Err("`name` was empty; it must be nonempty.".to_string())
+        Err(String::from("`name` was empty; it must be nonempty."))
     }
 }
 
